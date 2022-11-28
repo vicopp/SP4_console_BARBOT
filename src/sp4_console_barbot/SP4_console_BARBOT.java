efe30417c2db5f83bbba3733cda5fe1a2a4977ea
victor <victor@localhost>
2022-11-28 16:30:19 +0100

    commit fin seance

--- src/sp4_console_barbot/SP4_console_BARBOT.java
+++ src/sp4_console_barbot/SP4_console_BARBOT.java
@@ -14,11 +14,10 @@ public class SP4_console_BARBOT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        
-        
         // TODO code application logic here
+        
+             
+        
     }
     
 }
