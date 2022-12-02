18e24ea801e1ef4e4c991ce9af3386a12d804ef5
victor <victor@localhost>
2022-12-02 12:56:03 +0100

    commit plateau de jeu

--- src/PlateauDeJeu.java
+++ src/PlateauDeJeu.java
@@ -9,6 +9,17 @@
  */
 public class PlateauDeJeu {
     
+    CelluleDeGrille[][] grille = new CelluleDeGrille [6][7];
     
+    public PlateauDeJeu() {
+        
+        for (int i =0; i<6; i++) {
+            for (int j =0 ;j<7;j++) {
+            
+            grille[i][j] = new CelluleDeGrille();
+        }
+        }
+        
+    }
     
 }
