4d27fcd056afe66eff2bc3c150f41183b9e28fe6
victor <victor@localhost>
2022-12-03 14:11:02 +0100

    commit classe partie

--- src/Partie.java
+++ src/Partie.java
@@ -2,11 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.util.Random;
+import java.util.Scanner;      
 
 /**
  *
  * @author victor
  */
 public class Partie {
+    
+    private Joueur [] ListeJoueurs = new Joueur [2];
+    private Joueur joueurCourant;
+    private PlateauDeJeu plateau = new PlateauDeJeu ();
+    
+    
+    public Partie (Joueur joueur1, Joueur joueur2) {
+        
+        ListeJoueurs[0] = joueur1;
+        ListeJoueurs[1] = joueur2;
+        joueurCourant = null;
+        
+    }
+    
+    
+    
     
 }
