c5e3e07d47eadc85f0dd9da07f305ccb909d2435
victor <victor@localhost>
2022-12-07 12:34:06 +0100

    commit correction lancer de partie

--- src/sp4_console_barbot/SP4_console_BARBOT.java
+++ src/sp4_console_barbot/SP4_console_BARBOT.java
@@ -19,11 +19,18 @@ public class SP4_console_BARBOT {
         // TODO code application logic here
         
            
+        Joueur J1 = new Joueur  ("Line");
+        Joueur J2 = new Joueur  ("Vico");
+     
+        Partie partie = new Partie (J1,J2);
+        partie.initialiserPartie();
+        partie.lancerPartie();
+     
         
         
         
         
-        
+                
     }
     
 }
